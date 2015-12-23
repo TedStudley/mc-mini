335f493c9274296b03f0217103d64bf22268e4b2
Ted Studley <tedstudley@gmail.com>
2015-12-22 18:01:07 -0800

    Refactored `source/parser/parser.cpp`
      - Simplified logic in `queryX` functions to use `getX` with a
        fallback.
    
      - Changed all exception handling to use the new exception system.
    
      - Fixed some code formatting
    
      - Removed `using namespace std;` line, added `std::` qualifier
        wherever needed.

--- source/parser/parser.cpp
+++ source/parser/parser.cpp
@@ -7,208 +7,181 @@
 #include "parser/paramTree.h"
 #include "parser/parser.h"
 
-using namespace std;
-
-ParamParser::ParamParser (const string paramFile) {
-  ifstream paramStream(paramFile);
+ParamParser::ParamParser(const std::string paramFile) {
+  std::ifstream paramStream(paramFile);
 
   if (paramStream.fail()) {
     THROW_WITH_TRACE(RuntimeError() <<
             errmsg_info("Opening parameter file '" + paramFile + "' failed."));
   }
-  string lineBuf;
-  vector<string> parseBuf, paramBuf;
+  std::string lineBuf;
+  std::vector<std::string> parseBuf, paramBuf;
 
   this->treeBase = new ParamTree;
 
-  getline (paramStream, lineBuf);
+  getline(paramStream, lineBuf);
 
   while (!paramStream.eof()) {
     if (lineBuf != "") {
-      if (lineBuf.find_first_of("=") == string::npos) {
-        parseBuf.clear ();
-        parseBuf.push_back (lineBuf);
+      if (lineBuf.find_first_of("=") == std::string::npos) {
+        parseBuf.clear();
+        parseBuf.push_back(lineBuf);
       } else {
-        parseBuf = stringSplit(lineBuf ,"=");
+        parseBuf = stringSplit(lineBuf, "=");
       }
       paramBuf = stringSplit(parseBuf[0]);
 
       if (paramBuf[0] == "set") {
-        treeBase->addParam (paramBuf[1], parseBuf[1]);
+        treeBase->addParam(paramBuf[1], parseBuf[1]);
       } else if (paramBuf[0] == "enter") {
-        treeBase->addNode (paramBuf[1]);
-        treeBase->moveUp (paramBuf[1]);
+        treeBase->addNode(paramBuf[1]);
+        treeBase->moveUp(paramBuf[1]);
       } else if (paramBuf[0] == "leave") {
-        treeBase->moveDown ();
+        treeBase->moveDown();
       } else if (paramBuf[0][0] == '#') {
         // Do nothing
       } else {
-        cerr << "Unrecognized parameter type: " << lineBuf << endl;
+        std::cerr << "Unrecognized parameter type: '" << lineBuf << "'" << std::endl;
       }
     }
 
-    parseBuf.clear (); paramBuf.clear ();
-    getline (paramStream, lineBuf);
+    parseBuf.clear(); paramBuf.clear();
+    getline(paramStream, lineBuf);
   }
-  cerr << "Parameter tree built!" << endl << endl;
+  std::cerr << "Parameter tree built!" << std::endl << std::endl;
 }
 
-ParamParser::~ParamParser () {
+ParamParser::~ParamParser() {
   delete treeBase;
 }
 
-bool ParamParser::push (string key) {
-  try {
-    treeBase->moveUp (key);
-    return true;
-  } catch (exception& e) {
-    throw std::runtime_error(string(e.what()) + ": " + key + "; Exiting now.");
-  }
-  return false;
+bool ParamParser::push(std::string key) {
+  treeBase->moveUp(key);
+  return true;
 }
 
-bool ParamParser::tryPush (string key) {
+bool ParamParser::tryPush(std::string key) {
   try {
-    treeBase->moveUp (key);
+    treeBase->moveUp(key);
     return true;
-  } catch (exception& e) {
-    cerr << e.what() << ": " << key << "; Continuing." << endl;
+  } catch (KeyNotFoundException &e) {
+    std::cerr << *boost::get_error_info<errmsg_info>(e) << "; continuing anyway." << std::endl;
     return false;
   }
 }
 
-void ParamParser::pop () {
-  try {
-    treeBase->moveDown ();
-  } catch (exception& e) {
-    throw std::runtime_error(string(e.what()) + "; Exiting now.");
-  }
+void ParamParser::pop() {
+  treeBase->moveDown();
 }
 
-void ParamParser::getParamString (string key, string& result) {
-  try {
-    result = treeBase->getParam (key);
-  } catch (exception& e) {
-    throw std::runtime_error(string(e.what()) + ": " + key + "; Exiting now.");
-  }
-  cerr << key << " = " << result << endl; 
+void ParamParser::getParamString(std::string key, std::string &result) {
+  result = treeBase->getParam(key);
+  std::cerr << key << " = " << result << std::endl;
 }
 
-void ParamParser::queryParamString (string key, string& result, const string defaultVal = "") {
+void ParamParser::queryParamString(std::string key, std::string &result, const std::string defaultVal = "") {
   try {
-    result = treeBase->getParam (key);
-  } catch (exception& e) {
-    cerr << e.what() << ": " << key << "; Continuing." <<  endl;
+    getParamString(key, result);
+  } catch (KeyNotFoundException &e) {
+    std::cerr << *boost::get_error_info<errmsg_info>(e) << "; continuing with default." << std::endl;
     result = defaultVal;
-  }
-  cerr << key << " = " << result << endl; 
-}
-
-void ParamParser::getParamStringVect (string key, vector<string>& result) {
-  try {
-    result = stringSplit (treeBase->getParam (key));
-  } catch (exception& e) {
-    throw std::runtime_error(string(e.what()) + ": " + key + "; Exiting now.");
+    std::cerr << key << " = '" << result << "'" << std::endl;
   }
 }
 
-void ParamParser::queryParamStringVect (string key, vector<string>& result, const vector<string> defaultValue = vector<string>()) {
+void ParamParser::getParamStringVect(std::string key, std::vector<std::string> &result) {
+  std::string tempBuf = treeBase->getParam(key);
+  std::vector<std::string> tempVect = stringSplit(tempBuf);
+
+  result.clear();
+  for (std::string elem : tempVect) {
+    result.push_back(elem);
+  }
+}
+
+void ParamParser::queryParamStringVect(
+        std::string key,
+        std::vector<std::string> &result,
+        const std::vector<std::string> defaultValue = std::vector<std::string>()) {
   try {
-    result = stringSplit (treeBase->getParam (key));
-  } catch (exception& e) {
-    cerr << e.what() << ": " << key << "; Continuing." << endl;
+    getParamStringVect(key, result);
+  } catch (KeyNotFoundException &e) {
+    std::cerr << *boost::get_error_info<errmsg_info>(e) << "; continuing with default." << std::endl;
     result = defaultValue;
   }
 }
 
-void ParamParser::getParamInt (string key, int& result) {
-  try {
-    string tempBuf = treeBase->getParam (key);
+void ParamParser::getParamInt(std::string key, int &result) {
+  std::string tempBuf = treeBase->getParam(key);
+  result = stoi(tempBuf);
+  std::cerr << key << " = " << result << std::endl;
+}
 
-    result = stoi (tempBuf);
-  } catch (exception& e) {
-    throw std::runtime_error(string(e.what()) + ": " + key + "; Exiting now.");
+void ParamParser::queryParamInt(std::string key, int &result, const int defaultValue = 0) {
+  try {
+    getParamInt(key, result);
+  } catch (KeyNotFoundException &e) {
+    std::cerr << *boost::get_error_info<errmsg_info>(e) << "; continuing with default." << std::endl;
+    result = defaultValue;
+    std::cerr << key << " = " << result << std::endl;
   }
 }
 
-void ParamParser::queryParamInt (string key, int& result, const int defaultValue = 0) {
-  try {
-    string tempBuf = treeBase->getParam (key);
+void ParamParser::getParamIntVect(string key, std::vector<int> &result) {
+  std::string tempBuf = treeBase->getParam(key);
+  std::vector<std::string> tempVect = stringSplit(tempBuf);
 
-    result = stoi (tempBuf);
-  } catch (exception& e) {
-    cerr << e.what () << ": " << key << "; Continuing." << endl;
+  result.clear();
+  for (std::string elem : tempVect) {
+    result.push_back(stoi(elem));
+  }
+}
+
+void ParamParser::queryParamIntVect(
+        std::string key,
+        std::vector<int> &result,
+        const std::vector<int> defaultValue = std::vector<int>()) {
+  try {
+    getParamIntVect(key, result);
+  } catch (KeyNotFoundException &e) {
+    std::cerr << *boost::get_error_info<errmsg_info>(e) << "; continuing with default." << std::endl;
     result = defaultValue;
   }
 }
 
-void ParamParser::getParamIntVect (string key, vector<int>& result) {
-  
-  try {
-    string tempBuf = treeBase->getParam (key);
-    vector<string> tempVect = stringSplit (tempBuf);
-
-    for (vector<string>::iterator it = tempVect.begin(); it != tempVect.end(); ++it) {
-      result.push_back (stoi(*it));
-    }
-  } catch (exception& e) {
-    throw std::runtime_error(string(e.what()) + ": " + key + "; Exiting Now.");
-  }
+void ParamParser::getParamDouble(std::string key, double &result) {
+  string tempBuf = treeBase->getParam(key);
+  result = stod(tempBuf);
+  std::cerr << key << " = " << result << std::endl;
 }
 
-void ParamParser::queryParamIntVect (string key, vector<int>& result, const vector<int> defaultValue = vector<int> ()) {
+void ParamParser::queryParamDouble(std::string key, double &result, const double defaultValue = 0.0) {
   try {
-    string tempBuf = treeBase->getParam (key);
-    vector<string> tempVect = stringSplit (tempBuf);
-
-    for (vector<string>::iterator it = tempVect.begin(); it != tempVect.end(); ++it) result.push_back (stoi (*it));
-  } catch (exception& e) {
-    cerr << e.what () << ": " << key << "; Continuing." << endl;
+    getParamDouble(key, result);
+  } catch (KeyNotFoundException &e) {
+    std::cerr << *boost::get_error_info<errmsg_info>(e) << "; continuing with default." << std::endl;
     result = defaultValue;
+    std::cerr << key << " = " << result << std::endl;
   }
 }
 
-void ParamParser::getParamDouble (string key, double& result) {
-  try {
-    string tempBuf = treeBase->getParam (key);
-    result = stod (tempBuf);
-  } catch (exception& e) {
-    throw std::runtime_error(string(e.what()) + ": " + key + "; Exiting now.");
+void ParamParser::getParamDoubleVect(std::string key, std::vector<double> &result) {
+  std::string tempBuf = treeBase->getParam(key);
+  std::vector<std::string> tempVect = stringSplit(tempBuf);
+
+  for (std::string it : tempVect) {
+    result.push_back(stod(it));
   }
 }
 
-void ParamParser::queryParamDouble (string key, double& result, const double defaultValue = 0.0) {
+void ParamParser::queryParamDoubleVect(
+        std::string key,
+        std::vector<double> &result,
+        const std::vector<double> defaultValue = std::vector<double>()) {
   try {
-    string tempBuf = treeBase->getParam (key);
-    
-    result = stod (tempBuf);
-  } catch (exception& e) {
-    cerr << e.what () << ": " << key << "; Continuing." << endl;
-    result = defaultValue;
-  }
-}
-
-void ParamParser::getParamDoubleVect (string key, vector<double>& result) {
-  
-  try {
-    string tempBuf = treeBase->getParam (key);
-    vector<string> tempVect = stringSplit (tempBuf);
-
-    
-    for (vector<string>::iterator it = tempVect.begin(); it != tempVect.end(); ++it) result.push_back (stod(*it));
-  } catch (exception& e) {
-    throw std::runtime_error(string(e.what ()) + ": " + key + "; Exiting Now.");
-  }
-}
-
-void ParamParser::queryParamDoubleVect (string key, vector<double>& result, const vector<double> defaultValue = vector<double> ()) {
-  try {
-    string tempBuf = treeBase->getParam (key);
-    vector<string> tempVect = stringSplit (tempBuf);
-
-    for (vector<string>::iterator it = tempVect.begin(); it != tempVect.end(); ++it) result.push_back (stod (*it));
-  } catch (exception& e) {
-    cerr << e.what () << ": " << key << "; Continuing." << endl;
+    getParamDoubleVect(key, result);
+  } catch (KeyNotFoundException &e) {
+    cerr << *boost::get_error_info<errmsg_info>(e) << "; continuing with default." << std::endl;
     result = defaultValue;
   }
 }
