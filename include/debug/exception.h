7b345665e5b0959341af18dedaf2b1a8fcce7244
Ted Studley <tedstudley@gmail.com>
2015-12-22 17:18:30 -0800

    Added New Exception System
      - Added a new exception system deriving from boost::exception and
        std::exception
      - Added 'THROW_WITH_TRACE' macro to throw an exception with a failure
        stack trace attached.
      - Updated *all* exceptions to use the new exception system.
      - Fixed some formatting
      - Added a signal handler for segfaults to provide a debugging stack
        trace

---
+++ include/debug/exception.h
@@ -0,0 +1,35 @@
+#define THROW_WITH_TRACE(ex) \
+    BOOST_THROW_EXCEPTION( ex << trace_info(get_backtrace()) )
+
+#include <string>
+
+#include <boost/exception/all.hpp>
+
+#include "debug/backtrace.h"
+
+typedef boost::error_info<struct tag_errmsg, std::string> errmsg_info;
+typedef boost::error_info<struct tag_trace, std::string> trace_info;
+
+struct ExceptionBase : virtual std::exception, virtual boost::exception { };
+
+struct RuntimeError : public ExceptionBase {
+  const char *what() const noexcept { return "Runtime Error"; };
+};
+
+
+struct InvalidArgument : public RuntimeError {
+  const char *what() const noexcept { return "Argument Error"; };
+};
+
+struct ParserException : public RuntimeError {
+  const char *what() const noexcept { return "Parser Error"; };
+};
+
+struct KeyNotFoundException : public ParserException {
+  const char *what() const noexcept { return "Key not found"; };
+};
+
+struct ChildNotFoundException : public ParserException {
+  const char *what() const noexcept { return "Child not found"; };
+};
+typedef boost::error_info<struct tag_missing_key, std::string> missing_key_info;
--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,11 @@
 // Necessary for output in the command line.
 #include <iostream>
-// Necessary for throwing exceptions.
-#include <stdexcept>
 
+// Necessary for displaying exception information
+#include <boost/exception/diagnostic_information.hpp>
+
+// Exceptions and related typedefs
+#include "debug/exception.h"
 // Functions and data structures related to the geometry of the problem.
 #include "geometry/geometry.h"
 // Functions and data structures related to the implementation of numerical methods.
@@ -13,44 +16,53 @@
 #include "parser/parser.h"
 
 int main(int argc, char ** argv) {
+  // Install the segfault handler with backtrace
+  signal(SIGSEGV, handler);
 
-// The valid command line usage is "./mc-mini <parameter file>". Otherwise, throw an exception.
-  if (argc == 1) {
-    throw std::invalid_argument("usage: " + std::string{argv[0]} + " <parameter file>.");
-  }
+  try {
+    // The valid command line usage is "./mc-mini <parameter file>". Otherwise, throw an exception.
+    if (argc == 1) {
+      THROW_WITH_TRACE(InvalidArgument() <<
+              errmsg_info("usage: " + static_cast<std::string>(argv[0]) + " <parameter file>."));
+    }
 
-// Initialize the parser with the specified parameter file.
-  ParamParser parser(std::string{argv[1]});
-// Initialize geometry parameters.
-  GeometryStructure geometry (parser);
-// Initialize parameters related to the implementation of the employed numerical method.
-  ProblemStructure  problem  (parser, geometry);
-// Initialize parameters related to output structure.
-  OutputStructure   output   (parser, geometry, problem);
+    // Initialize the parser with the specified parameter file.
+    ParamParser parser(static_cast<std::string>(argv[1]));
+    // Initialize geometry parameters.
+    GeometryStructure geometry (parser);
+    // Initialize parameters related to the implementation of the employed numerical method.
+    ProblemStructure  problem  (parser, geometry);
+    // Initialize parameters related to output structure.
+    OutputStructure   output   (parser, geometry, problem);
 
-// Initialize the initial data for the problem to be solved.
-  problem.initializeProblem();
+    // Initialize the initial data for the problem to be solved.
+    problem.initializeProblem();
 
-// Main loop where computations are made and data is output for each timestep of the problem.
-  do
-  {
-    // 1. Solve Stokes equations.
+    // Main loop where computations are made and data is output for each timestep of the problem.
+    do {
+      // 1. Solve Stokes equations.
+      problem.solveStokes();
+      // 2. Initialize the right hand side (forcing terms).
+      problem.updateForcingTerms();
+      // 3. Recalculate time step.
+      problem.recalculateTimestep();
+      // 4. Output the solution data.
+      output.outputData (problem.getTimestepNumber());
+      // 5. Solve advection-diffusion equation.
+      problem.solveAdvectionDiffusion();
+      // 6. Output which time step is being computed.
+      std::cout << "Timestep: " << problem.getTimestepNumber() << ": t=" << problem.getTime() << std::endl;
+    } while (problem.advanceTimestep()); // Loop termination criterion: problem.getTimestepNumber() = end_timestep.
+
+    // Solve the Stokes equations.
     problem.solveStokes();
-    // 2. Initialize the right hand side (forcing terms).
+    // Update forcing terms
     problem.updateForcingTerms();
-    // 3. Recalculate time step.
-    problem.recalculateTimestep();
-    // 4. Output the solution data.
+    // Output the solution data.
     output.outputData (problem.getTimestepNumber());
-    // 5. Solve advection-diffusion equation.
-    problem.solveAdvectionDiffusion();
-    // 6. Output which time step is being computed.
-    std::cout << "<Timestep: " << problem.getTimestepNumber() << "; t=" << problem.getTime() << ">" << std::endl;
+  } catch (std::exception& e) {
+    std::cerr << boost::diagnostic_information(e);
   }
-  while (problem.advanceTimestep()); // Loop termination criterion: problem.getTimestepNumber() = end_timestep.
-
-  problem.solveStokes(); // Solve Stokes equations.
-  output.outputData (problem.getTimestepNumber()); // Output the solution data.
 
   return 0;
 }
